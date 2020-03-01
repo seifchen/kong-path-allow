4ecd133fd0119d18f59a33f740b0102a5bb4ddf8
chenxuefeng1 <chenxuefeng1@guazi.com>
2020-03-01 18:02:55 +0800

    remove schema field

--- src/schema.lua
+++ src/schema.lua
@@ -4,9 +4,6 @@ local typedefs = require "kong.db.schema.typedefs"
 return {
   name = "kong-path-whitelist",
   fields = {
-    { consumer = typedefs.no_consumer },
-    { run_on = typedefs.run_on_first },
-    { protocols = typedefs.protocols_http },
     { config = {
         type = "record",
         fields = {
